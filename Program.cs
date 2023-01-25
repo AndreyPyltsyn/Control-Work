0bad26bf973a9a59c25dae6fe6511251a93fd84f
Andrey <andpyl@mail.ru>
2023-01-25 15:36:52 +0300

    add check array

--- Program.cs
+++ Program.cs
@@ -1,8 +1,9 @@
 ﻿Console.WriteLine("Введите длинну массива");
 int arrayLength = int.Parse(Console.ReadLine()!);
 Console.WriteLine("Введите массив");
-string[] array = CreateArray(arrayLength);
-
+string[] array1 = CreateArray(arrayLength);
+string[] array2 = new string[array1.Length];
+CheckArray(array1, array2);
 
 
 
@@ -18,4 +19,18 @@ string[] CreateArray(int arrayLength)
         array[i] = Console.ReadLine()!;
     }
     return array;
+}
+
+string[] CheckArray(string[] array1, string[] array2)
+{
+    int j = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if (array1[i].Length <= 3)
+        {
+            array2[j] = array1[i];
+            j++;
+        }
+    }
+    return array2;
 }
