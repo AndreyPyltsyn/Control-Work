94a8da6b1c9bae97d7c56e3dc4f813cb5cb4b51d
Andrey <andpyl@mail.ru>
2023-01-25 15:17:49 +0300

    add code massive

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите длинну массива");
+int arrayLength = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите массив");
+string[] array = CreateArray(arrayLength);
+
+
+
+
+
+
+
+
+string[] CreateArray(int arrayLength)
+{
+    string[] array = new string[arrayLength];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = Console.ReadLine()!;
+    }
+    return array;
+}
